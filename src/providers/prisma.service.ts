aeeaff72bb2f6db20d9d38685b2004b33034cff6
Guido Serniotti <guido.serniotti.13@gmail.com>
2024-10-18 17:18:54 -0300

    feat: prisma service

---
+++ src/providers/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  readonly #logger: Logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+
+      this.#logger.debug('Connected to database correctly');
+    } catch (e) {
+      this.#logger.error(`Error connecting to database: ${e}`);
+      process.exit(0);
+    }
+  }
+}
