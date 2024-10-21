af835a49344409d161df47d5727e930143d779ae
Lucas <mazalucas@hotmail.com>
2024-10-21 15:53:38 -0300

    feat: password hashing

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/providers/prisma.service';
 import { Prisma, User } from '@prisma/client';
 import { IUsersService } from './users.interface';
+import { genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService implements IUsersService {
@@ -16,6 +17,13 @@ export class UsersService implements IUsersService {
   }
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({ data });
+    // Generar una salt para el hashing.
+    const salt = await genSalt();
+
+    // Hashear contraseña del usuario.
+    const password = await hash(data.password, salt);
+
+    // Crear el nuevo usuario con la constraseña hasheada.
+    return this.prisma.user.create({ data: { ...data, password } });
   }
 }
