fccd20254e33191059e8d2941eead304669bb793
Nicolas <mairone.nicolas@gmail.com>
2024-10-21 22:24:53 -0300

    feat: add app.module
    Se agrega el modulo de app con sus controllers, providers e imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { SizeController } from './size/size.controller';  
+import { ColorController } from "./color/color.controller";
+import { SizeService } from './size/size.service';
+import { ColorService } from "./color/color.service";      
+import { PrismaService } from './providers/prisma.service';      
+import { SizeModule } from './size/size.module';
+import { ColorModule } from "./color/color.module";
 
-@Module({})
+@Module({
+  controllers: [SizeController, ColorController], 
+  providers: [SizeService, ColorService, PrismaService],
+  imports: [SizeModule, ColorModule]
+})
 export class AppModule {}
