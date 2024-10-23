78f7ae99a200c1ee13e35df927a53561e16e8c74
Lucas <mazalucas@hotmail.com>
2024-10-23 14:15:33 -0300

    fix: import size and color modules on stock

--- src/modules/stock/stock.module.ts
+++ src/modules/stock/stock.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { SizeModule } from './size/size.module';
+import { ColorModule } from './color/color.module';
 
-@Module({})
+@Module({
+  imports: [SizeModule, ColorModule],
+})
 export class StockModule {}
