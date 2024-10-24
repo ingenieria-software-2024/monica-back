04ac8a564bf3122edf60abd6079926e49caac878
NicolasMairone <mairone.nicolas@gmail.com>
2024-10-23 21:44:12 -0300

    refactor: add VariantCategoryModule
    Se agrega el modulo de VariantCategory al main module.

--- src/modules/main.module.ts
+++ src/modules/main.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { StockModule } from './stock/stock.module';
 import { CategoryModule } from './category/category.module';
+import { VariantCategoryModule } from './variantCategory/variantCategory.module';
 
 @Module({
-  imports: [ProductModule, StockModule, CategoryModule],
+  imports: [ProductModule, StockModule, CategoryModule, VariantCategoryModule],
 })
 export class MainModule {}
