db912b609ed112291da1df72be8e168d2606f3e4
NicolasMairone <mairone.nicolas@gmail.com>
2024-10-23 21:41:35 -0300

    refactor: Delete size and color modules
    Se borraron estos modulos, ya que no sufrieron modificaciones en el esquema .

--- src/modules/stock/color/dto/assign-color.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
-
-export class AssignColorDto {
-  @IsNotEmpty()
-  @IsInt()
-  colorId: number;
-}
--- src/modules/stock/size/dto/assign-size.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
-
-export class AssignSizeDto {
-  @IsNotEmpty()
-  @IsInt()
-  sizeId: number;
-}
