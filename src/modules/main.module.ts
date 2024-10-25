9db360eb31b27cc6ece9c6ee58d4f85e3b12183a
Lucas <mazalucas@hotmail.com>
2024-10-25 14:05:33 -0300

    refactor: remove imports

--- src/modules/main.module.ts
+++ src/modules/main.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { StockModule } from './stock/stock.module';
 import { CategoryModule } from './category/category.module';
-import { VariantCategoryModule } from './category/variant/variant.category.module';
 
 @Module({
-  imports: [ProductModule, StockModule, CategoryModule, VariantCategoryModule],
+  imports: [ProductModule, StockModule, CategoryModule],
 })
 export class MainModule {}
