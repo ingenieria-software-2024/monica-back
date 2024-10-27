feda511bcb6cf65a9b9cc639605adc744836b19f
NicolasMairone <mairone.nicolas@gmail.com>
2024-10-27 19:56:52 -0300

    fix: updateVariant
    Se soluciono errores en el metodo para actualizar la variante para poder ejecutar el test.

--- src/modules/stock/stock.service.ts
+++ src/modules/stock/stock.service.ts
@@ -79,23 +79,22 @@ export class StockService implements IStockService {
    * @returns {Promise<ProductVariant>}
    */
   async getVariantById(id: number): Promise<ProductVariant> {
-    try {
-      return await this.prisma.productVariant.findUnique({
-        where: { id },
-        include: {
-          variantCategory: {
-            // Incluir la categoría de variante
-            select: {
-              name: true, // Obtener solo el nombre
-            },
+    const variant = await this.prisma.productVariant.findUnique({
+      where: { id },
+      include: {
+        variantCategory: {
+          select: {
+            name: true,
           },
         },
-      });
-    } catch (e) {
-      if (e instanceof Prisma.PrismaClientKnownRequestError)
-        throw new NotFoundException(`No se encontró la variante con ID: ${id}`);
-      this.#logger.error(`Failed to search variant by ID ${id}: ${e}`);
+      },
+    });
+
+    if (!variant) {
+      throw new NotFoundException(`No se encontró la variante con ID: ${id}`);
     }
+
+    return variant;
   }
 
   /**
