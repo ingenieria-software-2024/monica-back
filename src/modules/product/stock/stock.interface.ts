9b49e654ac3d44115bf4b5069f2b5e5b300245ea
Lucas <mazalucas@hotmail.com>
2024-11-04 20:10:06 -0300

    feat: add optional reasons to stock movements

--- src/modules/product/stock/stock.interface.ts
+++ src/modules/product/stock/stock.interface.ts
@@ -6,18 +6,28 @@ export interface IStockService {
    *
    * @param {number} productId Identificador para el producto.
    * @param {number} quantity Cantidad de este producto que se agregará al stock.
+   * @param {string} [reason] Opcional. Si es provista, una razon por la cual se agregó este stock.
    *
    * @returns {Promise<ProductVariant>} El producto con su stock actualizado.
    */
-  addStock(productId: number, quantity: number): Promise<ProductVariant>;
+  addStock(
+    productId: number,
+    quantity: number,
+    reason?: string,
+  ): Promise<ProductVariant>;
 
   /**
    * Remueve una cantidad específica de stock para un producto específico.
    *
    * @param {number} productId Identificador para el producto.
    * @param {number} [quantity] Opcional. Cantidad de este producto que se removerá del stock. Si no se especifica, se restará 1.
+   * @param {string} [reason] Opcional. Si es provista, una razon por la cual se removió este stock.
    *
    * @returns {Promise<ProductVariant>} El producto con su stock actualizado.
    */
-  removeStock(productId: number, quantity?: number): Promise<ProductVariant>;
+  removeStock(
+    productId: number,
+    quantity?: number,
+    reason?: string,
+  ): Promise<ProductVariant>;
 }
--- src/modules/product/stock/stock.service.ts
+++ src/modules/product/stock/stock.service.ts
@@ -1,19 +1,30 @@
 import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { IStockService } from './stock.interface';
-import { ProductVariant } from '@prisma/client';
+import { Prisma, ProductVariant } from '@prisma/client';
 import { ProductVariantService } from '../variants/product.variants.service';
 import { IProductVariantService } from '../variants/product.variants.interface';
+import { PrismaService } from 'src/providers/prisma.service';
 
 @Injectable()
 export class StockService implements IStockService {
   readonly #logger: Logger = new Logger(StockService.name);
 
+  /** Accesor para las operaciones CRUD de los logs de stock. */
+  readonly #stockLog: Prisma.HistoricStockLogDelegate;
+
   constructor(
+    private readonly prisma: PrismaService,
     @Inject(ProductVariantService)
     private readonly productVariants: IProductVariantService,
-  ) {}
+  ) {
+    this.#stockLog = prisma.historicStockLog;
+  }
 
-  async addStock(productId: number, quantity: number): Promise<ProductVariant> {
+  async addStock(
+    productId: number,
+    quantity: number,
+    reason?: string,
+  ): Promise<ProductVariant> {
     // Obtener el producto por ID.
     const product = await this.productVariants.getVariantById(productId);
 
@@ -27,12 +38,23 @@ export class StockService implements IStockService {
       stock: product.stock + quantity,
     });
 
+    // Si todo salio bien, crear una entrada de stock.
+    await this.#stockLog.create({
+      data: {
+        productVariantId: productId,
+        isIngress: true,
+        quantity,
+        reason: reason ?? Prisma.skip,
+      },
+    });
+
     return updatedProduct;
   }
 
   async removeStock(
     productId: number,
     quantity?: number,
+    reason?: string,
   ): Promise<ProductVariant> {
     // Obtener el producto por ID.
     const product = await this.productVariants.getVariantById(productId);
@@ -45,6 +67,16 @@ export class StockService implements IStockService {
     // Editar la cantidad de stock actual de la variante.
     const updatedProduct = await this.productVariants.updateVariant(productId, {
       stock: product.stock - (quantity ?? 1),
+    });
+
+    // Si todo salio bien, crear una entrada de stock.
+    await this.#stockLog.create({
+      data: {
+        productVariantId: productId,
+        isIngress: false,
+        quantity: quantity ?? 1,
+        reason: reason ?? Prisma.skip,
+      },
     });
 
     return updatedProduct;
