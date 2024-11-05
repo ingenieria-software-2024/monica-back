6d92f7f579d0a86c6061b189278fd1c99a8d45b7
Lucas <mazalucas@hotmail.com>
2024-11-05 15:55:50 -0300

    refactor: remove session deps

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Configura el middleware de sesión
-  app.use(
-    session({
-      secret: 'mi-secreto',
-      resave: false,
-      saveUninitialized: false,
-      cookie: { secure: false }, // Asegúrate de usar `true` en producción con HTTPS
-    }),
-  );
-
-  // Habilita el ValidationPipe para la validación automática de DTOs
-  app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
 }
 bootstrap();
