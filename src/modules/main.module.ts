741156343748578e7a6c3123cf72914bd1ecc180
Lucas <mazalucas@hotmail.com>
2024-11-06 19:41:31 -0300

    feat: add authmodule to dep injection tree

--- src/modules/main.module.ts
+++ src/modules/main.module.ts
@@ -3,8 +3,9 @@ import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { UsersModule } from './users/users.module';
 import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule, UsersModule, CartModule],
+  imports: [ProductModule, CategoryModule, UsersModule, CartModule, AuthModule],
 })
 export class MainModule {}
