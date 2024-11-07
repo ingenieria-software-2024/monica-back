df253bc26b71a8a3afcc5a2c60f8ee02db7b38e6
Guido Serniotti <guido.serniotti.13@gmail.com>
2024-11-07 15:15:24 -0300

    feat: user password testing

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -2,7 +2,7 @@ import { PrismaService } from '../../providers/prisma.service';
 import { UsersService } from './users.service';
 import { Test } from '@nestjs/testing';
 import { CreateUserDto } from './dto/create-user.dto';
-import { isEmail } from 'class-validator';
+import { isEmail, isStrongPassword } from 'class-validator';
 
 describe('Test Unitarios de UsersModule', () => {
   beforeAll(async () => {
@@ -31,6 +31,64 @@ describe('Test Unitarios de UsersModule', () => {
       };
 
       expect(isEmail(dto.email)).toBe(true);
+    });
+  });
+
+  describe('Validación de contraseña de usuario', () => {
+    it('La contraseña es demasiado corta', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: '12345',
+      };
+
+      expect(isStrongPassword(dto.password, { minLength: 8 })).toBe(false);
+    });
+
+    it('La contraseña no contiene números', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: 'contraseña',
+      };
+      expect(isStrongPassword(dto.password, { minNumbers: 1 })).toBe(false);
+    });
+
+    it('La contraseña no contiene letras mayúsculas', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: 'contraseña123',
+      };
+      expect(isStrongPassword(dto.password, { minUppercase: 1 })).toBe(false);
+    });
+
+    it('La contraseña no contiene letras minúsculas', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: 'CONTRASEÑA123',
+      };
+      expect(isStrongPassword(dto.password, { minLowercase: 1 })).toBe(false);
+    });
+
+    it('La contraseña no contiene caracteres especiales', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: 'Contraseña123',
+      };
+      expect(isStrongPassword(dto.password, { minSymbols: 1 })).toBe(false);
+    });
+
+    it('La contraseña cumple con los requisitos mínimos de seguridad', async () => {
+      const dto: CreateUserDto = {
+        username: undefined,
+        email: undefined,
+        password: 'Contraseña123!',
+      };
+      //Verifica que la contraseña cumple con los requisitos mínimos de seguridad definidos por "IsStrongPassword"
+      expect(isStrongPassword(dto.password)).toBe(true);
     });
   });
 });
